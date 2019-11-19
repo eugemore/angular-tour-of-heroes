f7f8f294055bb24f0e4f017e7b237a3ed58b848f
eugenio.morelli <eugenio.morelli@accenture.com>
2019-11-19 18:02:14 -0300

    added messae component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero-interface';
 import { HeroService } from '../hero.service';
-import { Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
